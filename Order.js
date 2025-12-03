582530ee4cf073f7f00cce3094bdfef1788a519d
Melisa Uslu <melisauslu463@gmail.com>
2025-12-03 19:09:45 +0300

    Update Order.js

--- Order.js
+++ Order.js
@@ -8,11 +8,11 @@ const orderSchema = new mongoose.Schema({
     },
     products: [
         {
-            id: { type: String, required: true }, // Ürün ID'si
+            id: { type: String, required: true }, 
             name: { type: String },
             price: { type: Number },
             quantity: { type: Number },
-            selectedSize: { type: String }, // Seçilen ayakkabı numarası
+            selectedSize: { type: String }, 
         },
     ],
     totalPrice: {
@@ -25,13 +25,14 @@ const orderSchema = new mongoose.Schema({
     },
     paymentStatus: {
         type: String,
-        default: "Başarılı", // Ödeme işlemi başarılı olarak varsayılır
+        default: "Başarılı", 
     },
     orderDate: {
         type: Date,
-        default: Date.now, // Sipariş oluşturulma zamanı
+        default: Date.now, 
     },
 });
 
 module.exports = mongoose.model("Order", orderSchema);
+
 
